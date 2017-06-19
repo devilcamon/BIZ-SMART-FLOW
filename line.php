7525bed3ada41200d6b9f6d4d81222adbe2cc42c
BIZ-SMART-FLOW <devilcamon@hotmail.com>
2017-06-19 22:34:20 +0700

    a

--- line.php
+++ line.php
@@ -8,7 +8,7 @@ https://api.line.me/v1/oauth/verify
  */
 
 $access_token = 'VGO54TpsjKQPB2fpcY02n2SbfETsnV6bNxZPdaeLgohtqwi7wnNl6xF+9zgA5xiv8xZhkUTBjg1Hgog0E23gvI86et1O1YHqbjJZw7FEzScidVC3J7no8vS6U0oFeeuYFei0IxF1tWcOFpTxJb5z5AdB04t89/1O/w1cDnyilFU=';
-
+/*
 $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
@@ -20,7 +20,7 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
 
-echo $result;
+echo $result;*/
 
 
 
