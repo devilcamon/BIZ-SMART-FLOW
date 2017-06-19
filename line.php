56fe8e0a0aea73028692d20f9525cc1f571b7345
BIZ-SMART-FLOW <devilcamon@hotmail.com>
2017-06-19 22:07:58 +0700

    a

--- line.php
+++ line.php
@@ -1,4 +1,25 @@
 <?php
 echo "Hello World. eiei na";
+
+/*
+ * curl -X GET \
+-H 'Authorization: Bearer {ENTER_ACCESS_TOKEN}' \
+https://api.line.me/v1/oauth/verify
+ */
+
+$access_token = 'VGO54TpsjKQPB2fpcY02n2SbfETsnV6bNxZPdaeLgohtqwi7wnNl6xF+9zgA5xiv8xZhkUTBjg1Hgog0E23gvI86et1O1YHqbjJZw7FEzScidVC3J7no8vS6U0oFeeuYFei0IxF1tWcOFpTxJb5z5AdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
 ?>
 
