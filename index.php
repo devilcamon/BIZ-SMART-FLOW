91edcbd34d3cfa1c2805b5850691f20eeac89b27
devilcamon <devilcamon@hotmail.com>
2017-06-23 17:14:06 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -11,25 +11,60 @@ function print_pre($txt)
 	print_r($txt);
 	echo "</pre>";
 }
-
-echo $txt = "มอบหมายงาน ทดสอบการมอบหมายงานให้คนอื่น ##BizSmartFlow @@NS!!";
-echo "<br>";
-$find_assign = strpos($txt, 'มอบหมายงาน');
-
-if($find_assign !== false)
+if(isset($_GET['txt']))
 {
-	echo $assign = iconv_substr($txt, 10, iconv_strlen($txt, 'utf-8'), 'utf-8');
+	$txt = urldecode($_GET['txt']);
 
-	preg_match_all("/(##)([a-zA-Z0-9_]+)( )/", $assign, $match_project, PREG_SET_ORDER);
-	preg_match_all("/(@@)([a-zA-Z0-9_]+)(!!)/", $assign, $match_to, PREG_SET_ORDER);
+}
+	//$txt = "มอบหมายงาน ทดสอบการมอบหมายงานให้คนอื่น ##BizSmartFlow @@NS!!";
+echo $txt;
+?>
+	<form class="form-horizontal" enctype="application/x-www-form-urlencoded">
+		<div class="form-group">
+			<div class="col-xs-12">
+				<textarea name="txt" id="txt" rows="3" class="form-control" ><?php echo $txt; ?></textarea>
+			</div>
+		</div>
+		<div class="form-group">
+			<div class="col-xs-12">
+				<button class="btn btn-primary btn-lg">ส่ง</button>
+			</div>
+		</div>
+	</form>
+	<?php
+$xxx = assign_job_from_line($txt);
+print_pre($xxx);
 
-	$assign_project = $match_project[0][0];
-	$assign_to = $match_to[0][0];
+function assign_job_from_line($txt)
+{
+	$find_assign = strpos($txt, 'มอบหมายงาน');
 
-	$array_search = array($assign_project, $assign_to);
-	$array_replace = array('', '');
-	echo "<br>";
-	echo $assign_detail = trim(str_replace($array_search, $array_replace, $assign));
+	if($find_assign !== false)
+	{
+		$assign = iconv_substr($txt, 10, iconv_strlen($txt, 'utf-8'), 'utf-8');
+
+		preg_match_all("/(##)([a-zA-Z0-9_]+)( )/", $assign, $match_project, PREG_SET_ORDER);
+		preg_match_all("/(@@)([a-zA-Z0-9_]+)(!!)/", $assign, $match_to, PREG_SET_ORDER);
+
+		$raw_assign_project = $match_project[0][0];
+		$raw_assign_to = $match_to[0][0];
+		$assign_project = $match_project[0][2];
+		$assign_to = $match_to[0][2];
+
+		$array_search = array($raw_assign_project, $raw_assign_to);
+		$array_replace = array('', '');
+		$assign_detail = trim(str_replace($array_search, $array_replace, $assign));
+
+		$data['assign_project'] = $assign_project;
+		$data['assign_to'] = $assign_to;
+		$data['assign_detail'] = $assign_detail;
+
+		return $data;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 ?>
